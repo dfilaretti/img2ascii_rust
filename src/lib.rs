691e2888a3bea49168a5cbe38430cf66ae235f28
dnf <dfilaretti@gmail.com>
2025-01-15 11:33:28 +0100

    generalise shrink_image to work with any image type

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,10 @@
 //! Actually, why not implementing both and giving user a choice?
 
 use clap::Parser;
-use image::imageops::{resize, FilterType};
+use image::{
+    imageops::{resize, FilterType},
+    GenericImageView, ImageBuffer, Pixel,
+};
 use std::error::Error;
 
 /// Simple program convert an image to ASCII art
@@ -38,13 +41,19 @@ fn lumi_8_to_char(lumi: u8) -> char {
 }
 
 /// Downsample an image by a factor of `block_size`
-/// TODO: this doesn't necessarily have to operate on `GreyImage` types; it could be more general (e.g. `DynamicImage`)
-fn shrink_image(img: image::GrayImage, block_size: u32, squeeze: u8) -> image::GrayImage {
+fn shrink_image<T: GenericImageView>(
+    img: &T,
+    block_size: u32,
+    squeeze: u8,
+) -> ImageBuffer<T::Pixel, Vec<<T::Pixel as Pixel>::Subpixel>>
+where
+    <T as GenericImageView>::Pixel: 'static
+{
     let (width, height) = img.dimensions();
     let (new_width, new_height) = (width / block_size, height / block_size);
 
     resize(
-        &img,
+        img,
         new_width * squeeze as u32,
         new_height,
         FilterType::Lanczos3,
@@ -81,7 +90,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // get a smaller image (downsample)
     // TODO: the block_size should not be fixed, but based on the image size (and the desired output size)
     let block_size = 32;
-    let img_smaller = shrink_image(img, block_size, config.squeeze);
+    let img_smaller = shrink_image(&img, block_size, config.squeeze);
 
     println!("Reduced image size by a factor of {}x", block_size);
     println!(
