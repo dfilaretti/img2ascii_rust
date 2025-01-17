b152f1ec87a9aa08359ae6aa96ccf3177484ffe9
dnf <dfilaretti@gmail.com>
2025-01-17 08:45:02 +0100

    add command line option to chose method for adjusting aspect ratio

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # img2ascii
 //!
-//! Convert pictures into ASCII art, allowing to specify the width of the output (in characters).
+//! Convert pictures into ASCII art, allowing users to specify the desired width of the output (in characters).
 //!
 
 use clap::{Parser, ValueEnum};
@@ -10,7 +10,7 @@ use image::{
 };
 use std::error::Error;
 
-/// Simple program convert an image to ASCII art
+/// Convert an image file to ASCII art
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Config {
@@ -26,7 +26,7 @@ pub struct Config {
     #[arg(short, long, default_value_t = 2)]
     squeeze: u8,
 
-    /// Display some debug information
+    /// Display debug information
     #[arg(short, long, default_value_t = false)]
     verbose: bool,
 
@@ -35,24 +35,24 @@ pub struct Config {
     correction_mode: CorrectionMode,
 }
 
-/// Mapping an (grey scale) image to ASCII pixel-by-pixel will usually lead to a distorted image, since 
-/// the aspect ratio of the characters is not the same as the pixels (i.e. we can think of pixels as being 
+/// Mapping an (grey scale) image to ASCII pixel-by-pixel will usually lead to a distorted image, since
+/// the aspect ratio of the characters is not the same as the pixels (i.e. we can think of pixels as being
 /// perfect squares while characters are usually taller than they are wide). This can be corrected in different
-/// way, and we offer two here. 
-/// 
+/// way, and we offer two here.
+///
 /// # RepeatChars
-/// 
+///
 /// This is the simplest method, where we simply repeat each character 'n' times (horizontally). Since characters
 /// are taller then wide, we can repeat each character 2 or 3 times to "build a square" (or close to it).
-/// 
+///
 /// # Stretch
-/// 
+///
 /// Here me map pixels to characters 1:1 (i.e. each pixel is represented by a single character) but in order to
 /// fix the aspect ratio we stretch the image horizontally by a factor of 2 or 3 (before generating the ASCII art).
 #[derive(ValueEnum, Debug, Clone)]
 enum CorrectionMode {
     Stretch,
-    RepeatChars,
+    Repeat,
 }
 
 /// Convert a luminance value to a character
@@ -80,20 +80,31 @@ where
     let new_width = (img.dimensions().0 as f32 / ratio) as u32;
     let new_height = (img.dimensions().1 as f32 / ratio) as u32;
 
+    let width_mult_factor = match config.correction_mode {
+        CorrectionMode::Stretch => config.squeeze,
+        _ => 1,
+    };
+
     resize(
         img,
-        new_width * config.squeeze as u32,
+        new_width * width_mult_factor as u32,
         new_height,
         FilterType::Lanczos3,
     )
 }
 
 /// Convert an image to ASCII art
-fn img_to_ascii(img: image::GrayImage) -> String {
+fn img_to_ascii(img: image::GrayImage, config: &Config) -> String {
+    // repeat each char n times horizontally to fix aspect ratio issue
+    let horiz_repeat_count = match config.correction_mode {
+        CorrectionMode::Repeat => config.squeeze,
+        _ => 1,
+    } as usize;
+
     img.enumerate_rows()
         // map each row to a string of ASCII characters (terminating with a newline)
         .map(|(_, row)| {
-            row.map(|(_, _, lumi)| repeat_char(lumi_8_to_char(lumi.0[0]), 1))
+            row.map(|(_, _, lumi)| repeat_char(lumi_8_to_char(lumi.0[0]), horiz_repeat_count))
                 .collect::<String>()
                 + "\n"
         })
@@ -101,8 +112,7 @@ fn img_to_ascii(img: image::GrayImage) -> String {
         .collect::<String>()
 }
 
-/// Repeat a character `c` `times` times
-/// TODO: currently not using this. How about allowing users to chose whether to use this or the image stretch method instead?
+/// Repeat a character `c` times
 pub fn repeat_char(c: char, times: usize) -> String {
     std::iter::repeat(c).take(times).collect()
 }
@@ -115,6 +125,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     if config.verbose {
         println!("Loaded image: {}", config.input_file);
         println!("Original image dimensions: {:?}", img.dimensions());
+        println!(
+            "Desired ASCII art width (in characters): {}",
+            config.width_char
+        );
+        println!(
+            "Aspect ratio correction method: {:?}",
+            config.correction_mode
+        )
     }
 
     // get a smaller image (downsample)
@@ -131,7 +149,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     // generate and print ASCII art
-    println!("{}", img_to_ascii(img_smaller));
+    println!("{}", img_to_ascii(img_smaller, &config));
 
     Ok(())
 }
